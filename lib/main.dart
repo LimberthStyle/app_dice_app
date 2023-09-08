fcead9f5111954720c7188299d3b097d81095d02
LimberthStyle <202001421b@utea.edu.pe>
2023-09-08 08:17:30 -0500

    DADOS_ALEATORIOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -14,7 +16,15 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int dadoIzq = 6;
+  int dadoDer = 6;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,16 +40,24 @@ class HomePage extends StatelessWidget {
           children: [
             Expanded(
               flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset("assets/images/dice2.png"),
+              child: TextButton(
+                onPressed: () {
+                  dadoIzq = Random().nextInt(6) + 1;
+                  dadoDer = Random().nextInt(6) + 1;
+                  setState(() {});
+                },
+                child: Image.asset("assets/images/dice$dadoIzq.png"),
               ),
             ),
             Expanded(
               flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset("assets/images/dice1.png"),
+              child: TextButton(
+                onPressed: () {
+                  dadoIzq = Random().nextInt(6) + 1;
+                  dadoDer = Random().nextInt(6) + 1;
+                  setState(() {});
+                },
+                child: Image.asset("assets/images/dice$dadoDer.png"),
               ),
             ),
           ],
